a1c8d4976e91392402a0cb44eb4e667fafaac60c
kos-d <dovhaichuk.kostiantyn@gmail.com>
2022-08-13 17:10:40 +0300

    hw5 hw6

---
+++ HW5/src/main/java/com/cursor/hw5/HW5.java
@@ -0,0 +1,8 @@
+package com.cursor.hw5;
+
+public class HW5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HW6/src/main/java/com/cursor/hw6/HW6.java
@@ -0,0 +1,8 @@
+package com.cursor.hw6;
+
+public class HW6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
