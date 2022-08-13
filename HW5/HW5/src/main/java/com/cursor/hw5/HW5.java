b371463b516a78a974acfdd7863b20d81de5533e
kos-d <dovhaichuk.kostiantyn@gmail.com>
2022-08-13 16:40:33 +0300

    edit hw5

--- HW5/HW5/src/main/java/com/cursor/hw5/HW5.java
+++ HW5/HW5/src/main/java/com/cursor/hw5/HW5.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.cursor.hw5;
+
+
 
 /**
  *
